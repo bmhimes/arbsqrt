45de77edf6cb355ab07f743fcb4de977e4e12fcd
Bryan Himes <bhimes@abanalytix.com>
2016-02-23 16:58:15 -0600

    Updated tests.

--- test/arbsqrt/core_test.clj
+++ test/arbsqrt/core_test.clj
@@ -9,8 +9,8 @@
 			  cli-errors (:errors parsed-opts)
 			  parsed-square (:square (:options parsed-opts))
 			  parsed-precision (:precision (:options parsed-opts))]
-			  (is (= 12.0 parsed-square))
-			  (is (= 0.01 parsed-precision))
+			  (is (= 12.0M parsed-square))
+			  (is (= 0.01M parsed-precision))
 			  (is (= nil cli-errors)))))
 
 (deftest java-interop
@@ -19,9 +19,9 @@
 
 (deftest utility-fns
   (testing "Testing error functions."
-  	(is (= (abs-pct-error 100 101) 0.01))
-  	(is (= (abs-pct-error 50 100) 1.0))
-  	(is (= (abs-pct-error 100 50) 0.5)))
+  	(is (= (abs-pct-error 100 101) 1/100))
+  	(is (= (abs-pct-error 50 100) 1))
+  	(is (= (abs-pct-error 100 50) 1/2)))
   (testing "Testing formatting functions."
   	(is (= "100.00%" (fmt-pct 1.0)))
   	(is (= "1.00%" (fmt-pct 0.01)))
@@ -32,9 +32,9 @@
   	(is (= 3.0 (calc-precision-scale 0.005)))
   	(is (= 3.0 (calc-precision-scale 0.001))))
   (testing "Ratio to Big Decimal conversion functions."
-  	(is (= 0.33M (ratio-to-big-dec (/ 1 3) 2)))))
+  	(is (= 0.33M (ratio-to-big-dec 1/3 2)))))
 
 (deftest core-fns
   (testing "Testing square estimate iteration."
-  	(is (= 4.0 (sqrt-est 12 6)))
-  	(is (= 3.5 (sqrt-est 12 4)))))
+  	(is (= 4 (sqrt-est 12 6)))
+  	(is (= 7/2 (sqrt-est 12 4)))))
