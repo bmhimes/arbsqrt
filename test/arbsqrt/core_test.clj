7b569ae895b8fa64049e7ea194410bb97280e032
Bryan Himes <bhimes@abanalytix.com>
2016-02-23 10:36:50 -0600

    Added test for Java math functions.

--- test/arbsqrt/core_test.clj
+++ test/arbsqrt/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [arbsqrt.core :refer :all]))
 
+(deftest java-interop
+	(testing "Testing Java math functions.")
+		(is (= 1 (java.lang.Math/abs -1))))
+
 (deftest utility-fns
   (testing "Testing utility functions."
   	(is (= (abs-pct-error 100 101) 0.01))
