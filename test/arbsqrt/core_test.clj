12a17ef9a83143cde0bb3ce065f6af72b30c0ff4
Bryan Himes <bhimes@abanalytix.com>
2016-02-23 00:19:19 -0600

    Added test for root estimate iteration.

--- test/arbsqrt/core_test.clj
+++ test/arbsqrt/core_test.clj
@@ -5,3 +5,7 @@
 (deftest utility-fns
   (testing "Testing utility functions."
   	(is (= (pct-error 100 101) -0.01))))
+
+(deftest core-fns
+  (testing "Testing square estimate iteration."
+  	(is (= 4 (sqrt-est 12 6)))))
