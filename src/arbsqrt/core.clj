ba2657ec6e512731280597c5eb5cae6d1a7f7ade
Bryan Himes <bhimes@abanalytix.com>
2016-02-23 00:12:32 -0600

    Added function to estimate root to a precision.

--- src/arbsqrt/core.clj
+++ src/arbsqrt/core.clj
@@ -10,6 +10,16 @@
 		  new-est (/ (+ old-est dividend) 2)]
 		  new-est))
 
+(defn precision-est
+	[square pct-precision]
+	(loop [new-est (sqrt-est square (/ square 2))]
+		(let [square-est (* new-est new-est)
+			  square-diff (- square-est square)
+			  error (/ square-diff square-est)])
+		(if (<= error pct-precision)
+			new-est
+			(recur new-est))))
+
 (defn -main [& args]
   (println "Hello, World!")
   (let [square 12
