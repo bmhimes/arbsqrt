e7649e830ec8a5d831826372c3cce7efaf06f1b4
Bryan Himes <bhimes@abanalytix.com>
2016-02-22 23:15:37 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to arbsqrt
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/arbsqrt/core_test.clj
@@ -0,0 +1,7 @@
+(ns arbsqrt.core-test
+  (:require [clojure.test :refer :all]
+            [arbsqrt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
