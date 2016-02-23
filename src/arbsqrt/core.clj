c02f19ff89f23fbf9961ad2309c904a534f13c43
Bryan Himes <bhimes@abanalytix.com>
2016-02-22 23:32:10 -0600

    Added function to perform estimate iteration. Added hard-coded args.

--- src/arbsqrt/core.clj
+++ src/arbsqrt/core.clj
@@ -1,4 +1,13 @@
 (ns arbsqrt.core)
 
+(defn sqrt-est
+	[square old-est]
+	(let [dividend (/ square old-est)
+		  new-est (/ (+ old-est dividend) 2)]
+		  new-est))
+
 (defn -main [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (let [square 12
+  		first-est (/ square 2)]
+  		(println (sqrt-est square first-est))))
