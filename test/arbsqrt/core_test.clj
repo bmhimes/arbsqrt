1f54a6bbec5c708df37bd90c0897e26273a25721
Bryan Himes <bhimes@abanalytix.com>
2016-02-23 11:28:34 -0600

    Added CLI option parsing tests.

--- test/arbsqrt/core_test.clj
+++ test/arbsqrt/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [arbsqrt.core :refer :all]))
 
+(deftest cli-opts
+	(testing "Valid CLI option parsing."
+		(let [cli-args ["-s" "12" "-p" "0.01"]
+			  parsed-opts (clojure.tools.cli/parse-opts cli-args cli-options)
+			  cli-errors (:errors parsed-opts)
+			  parsed-square (:square (:options parsed-opts))
+			  parsed-precision (:precision (:options parsed-opts))]
+			  (is (= 12.0 parsed-square))
+			  (is (= 0.01 parsed-precision))
+			  (is (= nil cli-errors)))))
+
 (deftest java-interop
 	(testing "Testing Java math functions.")
 		(is (= 1 (java.lang.Math/abs -1))))
