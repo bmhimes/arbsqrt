048f1004ca1dc0491a1307e7580a3d5d42e32870
Bryan Himes <bhimes@abanalytix.com>
2016-02-24 23:24:37 -0600

    Updated project metadata.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject arbsqrt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject arbsqrt "1.0"
+  :description "Estimates the square root of a number."
+  :url "https://github.com/bmhimes/arbsqrt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
